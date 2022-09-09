7b9e1c31e92368f0376097a4e3bba4b9e525225c
Trent Creekmore <trent@lindows.org>
2022-09-09 12:03:19 -0500

    added code for FreeSwitch compile problem: mod_sndfiles

--- astpp_v5_install.sh
+++ astpp_v5_install.sh
@@ -389,6 +389,10 @@ make sounds-install moh-install
 make hd-sounds-install hd-moh-install
 make cd-sounds-install cd-moh-install
 
+sed -i "s/#formats\/mod_sndfile/formats\/mod_sndfile/g" /usr/src/$switch_version.-release
+
+make mod_sndfile-install
+
 #move the music into music/default directory
 #mkdir -p /usr/share/freeswitch/sounds/music/default
 #mv /usr/share/freeswitch/sounds/music/*000 /usr/share/freeswitch/sounds/music/default
