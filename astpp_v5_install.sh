78f24a6e48958df5712d820dfa04b34c0f7364bd
Trent Creekmore <trent@lindows.org>
2022-09-13 13:55:07 -0500

    FS Directory variable modified

--- astpp_v5_install.sh
+++ astpp_v5_install.sh
@@ -74,7 +74,7 @@ ASTPP_DATABASE_NAME="astpp"
 ASTPP_DB_USER="astppuser"
 
 #Freeswich Configuration
-FS_DIR=/usr/local/freeswitch/share/freeswitch
+FS_DIR=/usr/local/freeswitch/
 FS_SOUNDSDIR=${FS_DIR}/sounds/en/us/callie
 
 
